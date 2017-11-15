5a39a28ed5c6f37581a8accdd8fe645a7a5e83ff
Dmitrii Malyshev <seodma@gmail.com>
2017-11-15 10:50:53 -0500

    Update client groups

--- lib/tapclicks-api/service/client_groups.rb
+++ lib/tapclicks-api/service/client_groups.rb
@@ -4,8 +4,8 @@ module TapclicksApi
     def service_url
       "https://api.tapclicks.com/v2/clientgroups"
     end
-
-    def create(params={})
+    # client group will be updated if client_group_id passed
+    def create_update(params={})
       url = "https://#{@@app_domain}.tapclicks.com/app/dash/clientGroup/update_client_group/key/id7pusme0j3strek7evfmbtj01alkskv"
       response = request(:post, url, params, true)
       model = JSON.parse(response.body)
