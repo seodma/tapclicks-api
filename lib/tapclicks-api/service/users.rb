fb98652a9be878cc28e36613ca570768e54be065
Dmitrii Malyshev <seodma@gmail.com>
2018-08-06 22:31:30 -0400

    Add create_update method to users

--- lib/tapclicks-api/service/users.rb
+++ lib/tapclicks-api/service/users.rb
@@ -3,5 +3,16 @@ module TapclicksApi
     def service_url
       "https://api.tapclicks.com/v2/users"
     end
+
+    def create_update(params={})
+      url = "http://#{@custom_domain}/app/dash/user/update_user/key/bd990f3b550abd09d47969c5f537782f"
+      response = request(:post, url, params, true, false)
+      model = JSON.parse(response.body)
+      if !model["data"]["id"].nil? && model["status"]["success"]
+        return model["data"]["id"]
+      else
+        raise model["data"].join(",")
+      end
+    end
   end
 end
